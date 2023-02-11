e14a945c187d9e9c8e15e9f7c07ef321891b2129
liuhuiyuan <13201479182@163.com>
2023-02-11 21:09:46 +0800

    :white_check_mark: heap: test
    arguments test passed

--- test/heap/argument.test.ts
+++ test/heap/argument.test.ts
@@ -4,12 +4,79 @@ import global from '../global';
 /**
  * 堆构造函数参数校验
  */
-test(`${global.count}---heap:argument 实例化时argument可选`, () => {
+test(`${global.count}---heap:arguments 实例化时arguments可选`, () => {
     const heap = new Heap();
     expect(heap).toBeInstanceOf(Heap);
 });
 
-test(`${global.count}---heap:argument 实例化时argument支持number[]`, () => {
+test(`${global.count}---heap:arguments 实例化时arguments支持number[]`, () => {
     const heap = new Heap([3, 5, 1, 7, 4]);
     expect(heap).toBeInstanceOf(Heap);
+});
+
+test(`${global.count}---heap:arguments 实例化时arguments支持配置大堆`, () => {
+    const heap = new Heap([3, 5, 1, 7, 4], false);
+    expect(heap.data).toStrictEqual([7, 5, 1, 3, 4]);
+});
+
+test(`${global.count}---heap:arguments 实例化时arguments支持配置小堆`, () => {
+    const heap = new Heap([3, 5, 1, 7, 4], true);
+    expect(heap.data).toStrictEqual([1, 4, 3, 7, 5]);
+});
+
+test(`${global.count}---heap:arguments 实例化时arguments支持配置object[]和priority`, () => {
+    const heap = new Heap(
+        [
+            { name: '二号', age: 3 },
+            { name: '二号', age: 5 },
+            { name: '三号', age: 1 },
+            { name: '四号', age: 7 },
+            { name: '五号', age: 4 },
+        ],
+        false,
+        'age',
+    );
+    expect(heap).toBeInstanceOf(Heap);
+});
+
+test(`${global.count}---heap:arguments 实例化时arguments支持配置object[]和大堆和priority`, () => {
+    const heap = new Heap(
+        [
+            { name: '二号', age: 3 },
+            { name: '二号', age: 5 },
+            { name: '三号', age: 1 },
+            { name: '四号', age: 7 },
+            { name: '五号', age: 4 },
+        ],
+        false,
+        'age',
+    );
+    expect(heap.data).toStrictEqual([
+        { name: '四号', age: 7 },
+        { name: '二号', age: 5 },
+        { name: '三号', age: 1 },
+        { name: '二号', age: 3 },
+        { name: '五号', age: 4 },
+    ]);
+});
+
+test(`${global.count}---heap:arguments 实例化时arguments支持配置object[]和小堆和priority`, () => {
+    const heap = new Heap(
+        [
+            { name: '二号', age: 3 },
+            { name: '二号', age: 5 },
+            { name: '三号', age: 1 },
+            { name: '四号', age: 7 },
+            { name: '五号', age: 4 },
+        ],
+        true,
+        'age',
+    );
+    expect(heap.data).toStrictEqual([
+        { name: '三号', age: 1 },
+        { name: '五号', age: 4 },
+        { name: '二号', age: 3 },
+        { name: '四号', age: 7 },
+        { name: '二号', age: 5 },
+    ]);
 });
